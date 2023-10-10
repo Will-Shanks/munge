a0d69a1e0976149815684d066290976ecc0a1dda
Will Shanks <shanks@ucar.edu>
2023-10-09 18:52:58 -0600

    clippy and cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use munge_sys;
-use std::ffi::{self,CString, CStr};
+use std::ffi::{self, CStr, CString};
 use std::ptr;
-use libc;
 
 fn get_err(err: munge_sys::munge_err) -> String {
     //err_msg should not be freed per munge api docs
-    let err_msg = unsafe{munge_sys::munge_strerror(err)};
-    let msg = unsafe{CStr::from_ptr(err_msg)}.to_str().unwrap().to_string();
+    let err_msg = unsafe { munge_sys::munge_strerror(err) };
+    let msg = unsafe { CStr::from_ptr(err_msg) }
+        .to_str()
+        .unwrap()
+        .to_string();
     msg
 }
 
@@ -23,12 +24,15 @@ pub fn munge(msg: &str) -> Result<String, String> {
     let mut cred: *mut ffi::c_char = ptr::null_mut();
     let len: ffi::c_int = msg.len().try_into().unwrap();
     let buf: *const ffi::c_void = CString::new(msg).unwrap().into_raw() as *const ffi::c_void;
-    let err = unsafe{munge_sys::munge_encode(&mut cred, ptr::null_mut(), buf, len)};
+    let err = unsafe { munge_sys::munge_encode(&mut cred, ptr::null_mut(), buf, len) };
     if err != 0 {
         Err(get_err(err))
-    }else {
-        let resp = Ok(unsafe{CStr::from_ptr(cred as *const i8)}.to_str().unwrap().to_string());
-        unsafe{libc::free(cred as *mut ffi::c_void)};
+    } else {
+        let resp = Ok(unsafe { CStr::from_ptr(cred as *const i8) }
+            .to_str()
+            .unwrap()
+            .to_string());
+        unsafe { libc::free(cred as *mut ffi::c_void) };
         resp
     }
 }
@@ -47,12 +51,28 @@ pub fn unmunge(encoded_msg: String) -> Result<Message, String> {
     let mut uid: munge_sys::uid_t = 0;
     let mut gid: munge_sys::gid_t = 0;
 
-    let err = unsafe{munge_sys::munge_decode(cred, ptr::null_mut(), &mut dmsg, &mut len, &mut uid, &mut gid)};
+    let err = unsafe {
+        munge_sys::munge_decode(
+            cred,
+            ptr::null_mut(),
+            &mut dmsg,
+            &mut len,
+            &mut uid,
+            &mut gid,
+        )
+    };
     if err != 0 {
         Err(get_err(err))
     } else {
-        let resp = unsafe{CStr::from_ptr(dmsg as *const i8)}.to_str().unwrap().to_string();
-        unsafe{libc::free(dmsg)};
-        Ok(Message{msg: resp, uid, gid})
+        let resp = unsafe { CStr::from_ptr(dmsg as *const i8) }
+            .to_str()
+            .unwrap()
+            .to_string();
+        unsafe { libc::free(dmsg) };
+        Ok(Message {
+            msg: resp,
+            uid,
+            gid,
+        })
     }
 }
